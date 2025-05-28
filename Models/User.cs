81aa3e1a89e167256e155f8dd97456ba1dfd6f1a
Null <nullulnull0@gmail.com>
2025-05-28 11:59:01 +0400

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace DataVerification.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
