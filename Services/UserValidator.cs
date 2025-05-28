6dc6654e5bc7e12e2a4831b0f6a2ce593c561ac8
Null <nullulnull0@gmail.com>
2025-05-28 12:01:08 +0400

    Add UserValidator.cs and ValidationResult.cs

---
+++ Services/UserValidator.cs
@@ -0,0 +1,35 @@
+﻿using DataVerification.Models;
+using DataVerification.Rules;
+using DataVerification.Services;
+using System.Collections.Generic;
+
+namespace DataVerification.Services
+{
+    public class UserValidator
+    {
+        private readonly List<IValidationRule<string>> _nameRules;
+        private readonly List<IValidationRule<string>> _emailRules;
+        private readonly List<IValidationRule<string>> _phoneRules;
+        private readonly List<IValidationRule<string>> _passwordRules;
+
+        public UserValidator()
+        {
+            _nameRules = new List<IValidationRule<string>> { new NameValidation() };
+            _emailRules = new List<IValidationRule<string>> { new EmailValidation() };
+            _phoneRules = new List<IValidationRule<string>> { new PhoneValidation() };
+            _passwordRules = new List<IValidationRule<string>> { new PasswordValidation() };
+        }
+
+        public List<ValidationResult> Validate(User user)
+        {
+            var results = new List<ValidationResult>();
+
+            _nameRules.ForEach(r => results.Add(r.Validate(user.Name)));
+            _emailRules.ForEach(r => results.Add(r.Validate(user.Email)));
+            _phoneRules.ForEach(r => results.Add(r.Validate(user.Phone)));
+            _passwordRules.ForEach(r => results.Add(r.Validate(user.Password)));
+
+            return results;
+        }
+    }
+}
---
+++ Services/ValidationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace DataVerification.Services
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; }
+        public string Message { get; }
+
+        public ValidationResult(bool isValid, string message)
+        {
+            IsValid = isValid;
+            Message = message;
+        }
+    }
+}
